9e7f22729ded56a2719ac0877bcb0dc24f582f82
Edson Zandamela <edsonaguiar17@gmail.com>
2018-09-16 23:52:52 -0400

    docs: Align searchbar and Home_Logo to the center of page

---
+++ Main/MyApps.js
@@ -0,0 +1,21 @@
+/* When a user clicks on the picture, 
+toggle between hiding and showing the dropdown content */
+function MyFunction(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.toggler_button')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown_content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
